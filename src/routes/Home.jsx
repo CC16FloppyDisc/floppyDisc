912aebe45d473a2f3fe49e767bfbac7035bf8bae
dmacdermott <damianmacdermott@gmail.com>
2020-12-16 15:21:07 +0900

    added search func

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import SearchBar from "../components/SearchBar";
 import ProductList from "../components/ProductList";
 // import { Elements } from "@stripe/react-stripe-js";
 // import { loadStripe } from "@stripe/stripe-js";
@@ -8,7 +7,6 @@ function Home() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
       <ProductList />
     </div>
   );
