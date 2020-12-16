bb01fc707e86230287d928f6dc65d43153d15246
dmacdermott <damianmacdermott@gmail.com>
2020-12-16 14:50:48 +0900

    imgs fixed and added routes

---
+++ src/routes/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import ProductList from "../components/ProductList";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+
+function Home() {
+  return (
+    <div className="App">
+      <Header />
+      <SearchBar />
+      <ProductList />
+    </div>
+  );
+}
+
+export default Home;
