b7713bcde90f2e3355a218d747064c6a44c47b20
dmacdermott <damianmacdermott@gmail.com>
2020-12-16 17:42:18 +0900

    fixed kirby mistake

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -45,5 +45,5 @@ const Product = ({ product }) => {
     </div>
   );
 };
-//en.wikipedia.org/wiki/Kirby%27s_Adventure#/media/File:Kirby's_Adventure_Coverart.png
-https: export default Product;
+
+export default Product;
