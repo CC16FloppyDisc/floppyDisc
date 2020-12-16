487ae68c6be5acd8a6dbdaf2b76f7e3357be28f4
dmacdermott <damianmacdermott@gmail.com>
2020-12-16 10:51:32 +0900

    added seeded data to front end

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,44 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'tron',
-      user:     'Yuta',
-      password: ''
-    }
+      database: "tron",
+      user: "work",
+      password: "",
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'tron',
-      user:     'Yuta',
-      password: ''
+      database: "tron",
+      user: "work",
+      password: "",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'tron',
-      user:     'Yuta',
-      password: ''
+      database: "tron",
+      user: "work",
+      password: "",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "./Product";
+import axios from "axios";
 
 const ProductList = () => {
+  const [products, setProducts] = useState();
+
+  const getProducts = () =>
+    axios.get("/item/").then(res => setProducts(res.data));
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-      <Product />
+      {products && products.map(product => <Product product={product} />)}
     </div>
   );
 };
